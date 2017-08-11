904a839efac52c63cd1c634af881884481ff963f
Ilya Kovalenko <ilya.kovalenko@unitpay.ru>
2017-08-11 14:06:34 +0300

    Fix

--- unitpay/result.php
+++ unitpay/result.php
@@ -4,7 +4,7 @@
 	require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
 	include(GetLangFileName(dirname(__FILE__)."/", "/payment.php"));
-	
+
 	class UnitPayMessage
 	{
 		function getMessage($message)
@@ -15,7 +15,7 @@
 				)
 			));
 		}
-		
+
 		function getError($message)
 		{
 			return json_encode(array(
@@ -25,38 +25,39 @@
 			));
 		}
 	}
-	
-	try {
-	
-		list($method, $params) = array($_GET['method'], $_GET['params']);
-		
-		
-		$id = $params["account"];
-		CModule::IncludeModule('sale');
-		$arOrder = CSaleOrder::GetByID($id);
-		$sum = $arOrder["PRICE"];
-		
-		$currency = COption::GetOptionString('czebra.unitpay', 'curr_'.$arOrder["LID"], '');
-		if(strlen($currency)==0)
-			$currency = "RUB";
-		$projectId = COption::GetOptionString('czebra.unitpay', 'numb_'.$arOrder["LID"], '');	
 
-		if ((double)$params['orderSum'] != (double)$sum ||
-			$params['orderCurrency'] != $currency ||
-			$params['account'] != $id ||
-			$params['projectId'] != $projectId) 
-		{
-			throw new InvalidArgumentException('Order validation Error!');
+	try {
+
+		list($method, $params) = array($_GET['method'], $_GET['params']);
+
+
+		$id = $params["account"];
+		if(CModule::IncludeModule('sale')){
+			$arOrder = CSaleOrder::GetByID($id);
+			$sum = $arOrder["PRICE"];
+
+			$currency = COption::GetOptionString('czebra.unitpay', 'curr_'.$arOrder["LID"], '');
+			if(strlen($currency)==0)
+				$currency = "RUB";
+			$projectId = COption::GetOptionString('czebra.unitpay', 'numb_'.$arOrder["LID"], '');
+
+			if ((double)$params['orderSum'] != (double)$sum ||
+				$params['orderCurrency'] != $currency ||
+				$params['account'] != $id ||
+				$params['projectId'] != $projectId)
+			{
+				throw new InvalidArgumentException('Order validation Error!');
+			}
+
+
+			if ($method  == 'check')  { print UnitPayMessage::getMessage('Check Success'); }
+			elseif ($method == 'pay') {
+				CSaleOrder::PayOrder((int)$id, "Y");
+				print UnitPayMessage::getMessage('Pay Success');
+
+			}
 		}
-		
-		
-		if ($method  == 'check')  { print UnitPayMessage::getMessage('Check Success'); } 
-		elseif ($method == 'pay') { 
-			CSaleOrder::PayOrder((int)$id, "Y");
-			print UnitPayMessage::getMessage('Pay Success'); 
-			
-		}
-	
+
 	} catch (Exception $e) {
     print UnitPayMessage::getError($e->getMessage());
 }
