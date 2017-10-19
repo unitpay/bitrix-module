60142ae58b3e4887d49b66cf0a61b7ee2f770bac
Ilya Kovalenko <ilya.kovalenko@unitpay.ru>
2017-10-19 14:17:12 +0300

    Языковой файл

---
+++ handlers/lang/ru/result.php
@@ -0,0 +1,7 @@
+<?php
+
+global $MESS;
+
+$MESS['UNITPAY_CHECK_SUCCESS'] = 'Заказ можно оплатить';
+$MESS['UNITPAY_PAY_SUCCESS'] = 'Заказ успешно оплачен';
+$MESS['UNITPAY_UNKNOWN_METHOD'] = 'Неизвестный метод';
--- handlers/result.php
+++ handlers/result.php
@@ -1,5 +1,6 @@
 <?
 use \Bitrix\Main\Application;
+use \Bitrix\Main\Localization\Loc;
 
 define("NO_KEEP_STATISTIC", true);
 define("NOT_CHECK_PERMISSIONS", true);
@@ -8,6 +9,7 @@ define("STOP_STATISTICS", true);
 define('NO_AGENT_CHECK', true);
 define("DisableEventsCheck", true);
 require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
 
 if (CModule::IncludeModule("sale"))
 {
@@ -18,16 +20,16 @@ if (CModule::IncludeModule("sale"))
     $service = new Bitrix\Sale\PaySystem\Service($item);
     $result = $service->processRequest($request);
 
-    //print_r($result);
+    // print_r($request);
 
     if($result->isSuccess()){
 	$method = $request->get('method');
         if ($method  == 'check'){
-        	echo json_encode(array("result" => array("message" => 'Check Success')));
+        	echo json_encode(array("result" => array("message" => getMessage('UNITPAY_CHECK_SUCCESS'))));
         } elseif ($method == 'pay'){
-        	echo json_encode(array("result" => array("message" => 'Pay Success')));
+        	echo json_encode(array("result" => array("message" => getMessage('UNITPAY_PAY_SUCCESS'))));
         } else {
-			echo json_encode(array("error" => array("message" => 'No connect')));
+		echo json_encode(array("error" => array("message" => getMessage('UNITPAY_UNKNOWN_METHOD'))));
 	}
     } else {
 		$message = implode(";", $result->getErrorMessages());
